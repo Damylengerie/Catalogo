b31028806002c1ed5e6c4f5cad4948dd13103595
Damylengerie <kaykypereirat2717@gmail.com>
2024-05-20 10:31:29 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+     /*Menu hamburg em modo mobile*/
+     const navbar = document.querySelector(".navbar");
+     const menuButton = document.querySelector(".botao-menu");
+ 
+     menuButton.addEventListener('click', () =>{
+         navbar.classList.toggle("menu-hamburg")
+     });
+
+    
